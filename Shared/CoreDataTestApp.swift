7e6e3c8fffc298c8364b4576173076d7eedb6284
jnpdx <john@johnnastos.com>
2021-12-14 16:39:16 -0800

    Initial Commit

---
+++ Shared/CoreDataTestApp.swift
@@ -0,0 +1,20 @@
+//
+//  CoreDataTestApp.swift
+//  Shared
+//
+//  Created by John Nastos on 12/14/21.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataTestApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
