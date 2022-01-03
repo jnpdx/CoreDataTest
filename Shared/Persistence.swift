0c4f9d37d8327f057d16531f6e9e3d5ea9b786e4
jnpdx <john@johnnastos.com>
2022-01-02 17:14:03 -0800

    Reorganizing code

--- Shared/Persistence.swift
+++ Shared/Persistence.swift
@@ -9,30 +9,10 @@ import CoreData
 
 struct PersistenceController {
     static let shared = PersistenceController()
-    
-    static var preview: PersistenceController = {
-        let result = PersistenceController(inMemory: true)
-        let viewContext = result.container.viewContext
-        for _ in 0..<10 {
-            let newItem = MetronomeItem(context: viewContext)
-            newItem.timestamp = Date()
-        }
-        do {
-            try viewContext.save()
-        } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-        }
-        return result
-    }()
-    
     let container: NSPersistentCloudKitContainer
     
     init(inMemory: Bool = false) {
         container = NSPersistentCloudKitContainer(name: "CoreDataTest")
-        //TODO: add back in -- needs to work with widget
         container.viewContext.automaticallyMergesChangesFromParent = true
         
         let storeURL = URL.storeURL(for: "group.com.johnnastos.CoreDataTest", databaseName: "CoreDataTest")
@@ -47,7 +27,6 @@ struct PersistenceController {
         container.persistentStoreDescriptions = [storeDescription]
         print("StoreURL: ",storeURL)
 
-        
 #if DEBUG
         do {
             try container.initializeCloudKitSchema(options: [])
@@ -62,9 +41,6 @@ struct PersistenceController {
         }
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                
                 /*
                  Typical reasons for an error here include:
                  * The parent directory does not exist, cannot be created, or disallows writing.
@@ -79,8 +55,25 @@ struct PersistenceController {
     }
 }
 
-public extension URL {
+extension PersistenceController {
+    static var preview: PersistenceController = {
+        let result = PersistenceController(inMemory: true)
+        let viewContext = result.container.viewContext
+        for _ in 0..<10 {
+            let newItem = MetronomeItem(context: viewContext)
+            newItem.timestamp = Date()
+        }
+        do {
+            try viewContext.save()
+        } catch {
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+        return result
+    }()
+}
 
+public extension URL {
     /// Returns a URL for the given app group and database pointing to the sqlite database.
     static func storeURL(for appGroup: String, databaseName: String) -> URL {
         guard let fileContainer = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: appGroup) else {
